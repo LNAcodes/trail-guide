3d4693f00c771bc06ecd3ab1ece97d22742126f2
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-08-06 15:01:41 +0200

    wip: trailModel.ts

---
+++ src/models/trailModel.ts
@@ -0,0 +1,37 @@
+import { getDB } from "./db";
+
+export interface Trail {
+  id?: number;
+  region_id: number;
+  title: string;
+  slug: string;
+  difficulty: string;
+  distance_km: number;
+  description: string;
+  image_url: string;
+  created_at: number;
+  region_name: string;
+  region_country: string;
+}
+
+export async function getAllTrails(): Promise<Trail[]> {
+  const db = getDB();
+  /* return await db.all<Trail[]>("SELECT
+  trails.id, trails.region_id, trails.title, trails.slug,
+  trails.difficulty, trails.distance_km, trails.description,
+  trails.image_url, trails.created_at,
+  regions.name AS region_name,
+  regions.country AS region_country" FROM trails
+INNER JOIN regions ON trails.region_id = regions.id,);
+}
+instead of listing every column from trails > "shortcut" trails.*, and no collison with id (in both interfaces), because we don't call id, only name and country
+  */
+  return await db.all<Trail[]>(
+    "SELECT trails.*, regions.name * FROM trails INNER JOIN regions ON trails.region_id = regions.id",
+  );
+}
+
+export async function getTrailBySlug(slug: string): Promise<Trail | undefined> {
+  const db = getDB();
+  return await db.get<Trail>("SELECT * FROM trails WHERE slug = ? ", slug);
+}
